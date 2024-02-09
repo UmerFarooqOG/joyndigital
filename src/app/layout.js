2a95950bc6bdc3b9e4beb4288f84c02d1114ddf9
Zubair Ahmad <zubairahmad@joyngroup.us>
2024-02-09 12:04:11 +0500

    add speed teseter

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import { SpeedInsights } from '@vercel/speed-insights/next';
+ 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -19,6 +21,7 @@ export default function RootLayout({ children }) {
      
         {children}
         <Footer />
+        <SpeedInsights />
         </body>
     </html>
   )
