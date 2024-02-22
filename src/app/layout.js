429a0ab4f02b290185bc76d3ee8f9c2f3d091e07
Zubair Ahmad <zubairahmad@joyngroup.us>
2024-02-23 02:10:15 +0500

    ss

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,9 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 import Head from 'next/head'
 
 
-const metadata = {
+
+
+export const metadata = {
   title: 'Innovative Digital Solutions | Joyn Digital - Your IT Partner',
   description: 'Discover innovative digital solutions tailored to meet your business needs at Joyn Digital. Partner with us to transform your online presence and achieve your goals.',
   openGraph: {
@@ -15,7 +17,7 @@ const metadata = {
     description: 'Discover innovative digital solutions tailored to meet your business needs at Joyn Digital. Partner with us to transform your online presence and achieve your goals.',
     images: [
       {
-        url: '/opengraph.jpg', // Use the relative path to your image
+        url: '/opengraph.jpg', // Add your Open Graph image URL here
         width: 1200,
         height: 630,
         alt: 'Joyn Digital Open Graph Image'
@@ -25,8 +27,7 @@ const metadata = {
     "color-scheme": 'white gray only',
   },
   twitter: {
-    cardType: 'summary_large_image',
-    image: '/opengraph.jpg' // Use the relative path to your Twitter card image
+    cardType: 'summary_large_image'
   }
 };
 
@@ -35,35 +36,14 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en" className='scroll-smooth'>
-           <Head>
+      <Head>
+
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon.ico" />
-        <meta charSet="utf-8" />
+        <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-        {/* Include metadata for SEO */}
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        {metadata.openGraph && (
-          <>
-            <meta property="og:type" content={metadata.openGraph.type} />
-            <meta property="og:url" content={metadata.openGraph.url} />
-            <meta property="og:title" content={metadata.openGraph.title} />
-            <meta property="og:description" content={metadata.openGraph.description} />
-            {metadata.openGraph.images && metadata.openGraph.images.map((image, index) => (
-              <meta key={index} property="og:image" content={image.url} />
-            ))}
-            <meta name="theme-color" content={metadata.openGraph['theme-color']} />
-            <meta name="color-scheme" content={metadata.openGraph['color-scheme']} />
-          </>
-        )}
-        {metadata.twitter && (
-          <>
-            <meta name="twitter:card" content={metadata.twitter.cardType} />
-            <meta name="twitter:image" content={metadata.twitter.image} />
-          </>
-        )}
       </Head>
+
       <body  >
         <Navbar />
 
