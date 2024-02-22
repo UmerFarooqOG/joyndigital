8939dcf2a396075e345ab276b24e6f4f6c5f85cf
Zubair Ahmad <zubairahmad@joyngroup.us>
2024-02-22 12:32:39 +0500

    a

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,60 +1,74 @@
 
 'use client'
-import Link from 'next/link'
-import { protData } from '@/app/constant/NavLink'
-import ProtfolioCardReverse from '@/components/ProtCardReverse'
-import ProtfolioCard from '@/components/ProtfolioCard'
-import {motion} from 'framer-motion'
-import Hero from '@/components/Hero.jsx'
+import Link from 'next/link';
+import { protData } from '@/app/constant/NavLink';
+import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import dynamic from 'next/dynamic'; // Import dynamic function from next/dynamic
 
-const page = () => {
+
+const ProtfolioCardReverse = dynamic(() => import('@/components/ProtCardReverse') , {ssr:false});
+const ProtfolioCard = dynamic(() => import('@/components/ProtfolioCard') , {ssr:false});
+const Hero = dynamic(() => import('@/components/Hero.jsx') , {ssr:false});
+const Page = () => {
+  const [isMobile, setIsMobile] = useState(false);
   
-  const tranitiom = {duration:1,type:'spring'}
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const htini = !isMobile ? { x: 200, scale: 1 } : { x: 0, scale: 1 };
+  const htwhilein = !isMobile ? { x: 0, scale: 1 } : { x: 0, scale: 1 };
+  const transition = { duration: 2, type: 'spring' };
+
   return (
     <div className='overflow-hidden h-full'>
-    {/* ------- hero --------  */}
-    <section className='h-[70vh] xsm:h-[87vh] mx-auto'>
-   <Hero />
+      {/* Hero Section */}
+      <section className='h-[70vh] xsm:h-[87vh] mx-auto'>
+        <Hero />
+      </section>
 
-    </section>
+      {/* Text Section */}
+      <section className='w-full max-w-[1440px] h-full px-[20px] sm:px-[60px] lg:px-[90px] xlb:px-[120px] py-[56px] mx-auto'>
+        <motion.h2 initial={htini} whileInView={htwhilein} transition={transition} className='text-[24px] ssm:text-[28px] xsm:text-[30px] sm:text-[35px] mdd:text-[50px] lg:text-[60px] xll:text-[72px] leading-normal lg:leading-[100px] w-full text-[#2E2E2E] font-Montserrat font-bold'>
+          We let you build relations <b></b> through our tailored digital products.
+        </motion.h2>
+        <div className='w-full flex-col my-[30px] flex items-end justify-start sm:justify-end'>
+          <div className='w-full mdd:w-[60%]'>
+            <motion.p initial={htini} whileInView={htwhilein} transition={transition} className='text-[#7F7F7F] font-Noto text-[18px] md:text-[22px] xll:text-[24px] leading-normal sm:leading-[38px]'>
+              We create digital products and platforms that drive user engagement and redefine industries by developing an intimate knowledge of our clients and their audiences.
+            </motion.p>
+            <Link href={'/contact'}>
+              <motion.p className='my-[30px] inline-block bg-[#2957A7] hover:bg-transparent border-[#2957a7] hover:shadow-sm hover:text-black duration-[0.2s] border-[1px] font-Montserrat text-left font-semibold text-white rounded-[40px] xlb:rounded-[40px] text-[16px] mdd:text-[18px] xlb:text-[22px] leading-[50px] px-[25px]'>
+                Get in touch
+              </motion.p>
+            </Link>
+          </div>
+        </div>
+      </section>
 
-    {/* ---- --- text-section  */}
-    <section className='w-full max-w-[1440px] h-full px-[20px] sm:px-[60px] lg:px-[90px] xlb:px-[120px] py-[56px] mx-auto'>
-      <motion.h2  initial={{opacity : 0 , x:-200 ,  }}
-              whileInView={{opacity : 1,x:0 ,   }}
-              transition={{ duration: 1 ,type:'tween'}} className=' text-[24px] ssm:text-[28px] xsm:text-[30px] sm:text-[35px] mdd:text-[50px] lg:text-[60px] xll:text-[72px] leading-normal lg:leading-[100px] w-full text-[#2E2E2E] font-Montserrat font-bold'>We let you build relations <b></b> through our tailored digital products.</motion.h2>
-      <div className='w-full flex-col my-[30px] flex items-end justify-start sm:justify-end'>
-      <div className=' w-full mdd:w-[60%]'>
-      <motion.p initial={{opacity : 0 , x:200 ,scale:0.7  }}
-              whileInView={{opacity : 1,x:0,   scale:1    }}
-              transition={tranitiom} className='text-[#7F7F7F] font-Noto text-[18px] md:text-[22px] xll:text-[24px] leading-normal sm:leading-[38px] '>We create digital products and platforms that drive user engagement and redefine industries by developing an intimate knowledge of our clients and their audiences.</motion.p>
-      <Link href={'/contact'} ><motion.p className='my-[30px] inline-block bg-[#2957A7] hover:bg-transparent border-[#2957a7] hover:shadow-sm hover:text-black duration-[0.5s] border-[1px] font-Montserrat text-left font-semibold text-white rounded-[40px] xlb:rounded-[40px] text-[16px] mdd:text-[18px] xlb:text-[22px] leading-[50px] px-[25px]' initial={{opacity:0 , y:100}} whileInView={{opacity:1, y:0}} transition={tranitiom}>Get in touch</motion.p></Link>
-      </div>
-      </div>
-    </section>
+      {/* Work Section */}
+      <section className='w-full max-w-[1440px] min-h-[100vh] px-[20px] sm:px-[60px] lg:px-[90px] xlb:px-[120px] py-[2rem] mx-auto'>
+        <motion.h2 className='font-Montserrat text-[60px] font-[500] text-[#2e2e2e]'>Here’s our <span className='font-bold'>work</span> </motion.h2>
+        <div className='w-full flex mb-[10px] ssm:mb-[40px] flex-col items-center justify-center gap-[40px] h-full'>
+          {protData.map((item, index) => (
+            index % 2 === 0 ? (
+              <ProtfolioCard key={index} logo={item.logo} spanTitle={item.spanTitle} title={item.title} desc={item.desc} image={item.image} id={index + 1} isMobile={isMobile} />
+            ) : (
+              <ProtfolioCardReverse key={index} logo={item.logo} spanTitle={item.spanTitle} title={item.title} desc={item.desc} image={item.image} id={index + 1} isMobile={isMobile} />
+            )
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+};
 
-    {/* work-section  */}
-    <section className='w-full max-w-[1440px] min-h-[100vh] px-[20px] sm:px-[60px] lg:px-[90px] xlb:px-[120px] py-[2rem] mx-auto'>
-      <motion.h2 initial={{opacity : 0 , x:-200   }}
-              whileInView={{opacity : 1,x:0     }}
-              transition={{ duration: 1 ,type:'tween'}} className='font-Montserrat text-[60px] font-[500] text-[#2e2e2e]'>Here’s our <span className='font-bold'>work</span> </motion.h2>
-      <div className='w-full flex mb-[10px] ssm:mb-[40px] flex-col items-center justify-center gap-[40px] h-full'>
-        <ProtfolioCard logo={protData[0].logo} spanTitle={protData[0].spanTitle} title={protData[0].title} desc={protData[0].desc} image={protData[0].image} id={1} />
-        <ProtfolioCardReverse logo={protData[1].logo} spanTitle={protData[1].spanTitle} title={protData[1].title} desc={protData[1].desc} image={protData[1].image} id={2} />
-        <ProtfolioCard logo={protData[2].logo} spanTitle={protData[2].spanTitle} title={protData[2].title} desc={protData[2].desc} image={protData[2].image} id={3} />
-
-
-        {/* <motion.button  initial={{opacity : 0 , y:80 ,scale:0.6  }}
-              whileInView={{opacity : 1,y:0 ,scale:1   }}
-              transition={{ duration: 0 ,type:'tween'}} className='px-[30px] btn font-Montserrat bg-transparent font-semibold text-[#2957A7] hover:text-[#fff] py-[10px] text-[20px] rounded-[40px] border-[#2957A7] border-[1px]'>
-          see more work
-        </motion.button> */}
-      </div>
-
-    </section>
-
-  </div>
-  )
-}
-
-export default page
+export default Page;
--- src/components/ProtCardReverse.jsx
+++ src/components/ProtCardReverse.jsx
@@ -1,35 +1,29 @@
-'use client'
-import Image from 'next/image'
+import Image from 'next/image';
 import { motion } from 'framer-motion';
-
-import { FaLongArrowAltRight } from "react-icons/fa";
 import Link from 'next/link';
-const ProtfolioCardReverse = ({ logo, spanTitle, title, desc, image, id }) => {
-    const tranitiom = { duration: 4, type: 'spring' }
-    const blueGradient = {
-        background: 'linear-gradient(298deg, #951314 0%, #EB2022 100%)'
-    }
-    return (
-        <motion.div initial={{ opacity: 0, x: 200 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={tranitiom} className='flex flex-col mdd:flex-row-reverse my-[20px] rounded-[30px] h-auto  mdd:h-[420px] lg:h-[490px] xlg:h-[510px]  xll:h-[490px]  w-full relative items-center justify-center'>
 
+const ProtfolioCardReverse = ({ logo, spanTitle, title, desc, image, id, isMobile }) => {
+  const blueGradient = {
+    background: 'linear-gradient(298deg, #951314 0%, #EB2022 100%)'
+  };
 
-            <div className='rounded-[20px] rounded-s-[20px] mdd:rounded-s-[0px]  rounded-e-[30px]  h-full w-full my-[40px] mdd:w-[45%] flex flex-col gap-[16px] px-[20px] xsm:px-[20px] xlg:px-[30px] xll:px-[55px] py-[20px] items-start justify-center ssm:py-[60px]  bg-white '>
-                <Image src={logo} width={100} height={100} className='w-[120px] lg:w-[228px] h-[70px]' alt='Logo image' />
-                <h3 className=' text-[15px] leading-[38px] ssm:text-[18px] xsm:text-[22px] lg:text-[28px] font-normal font-Montserrat'><span className='text-[#EB2022] font-bold'>{spanTitle}</span>{title}</h3>
+  const initialAnimation = !isMobile ? { x: 200, scale: 1 } : { x: 0, scale: 1 };
+  const whileInViewAnimation = !isMobile ? { x: 0, scale: 1 } : { x: 0, scale: 1 };
+  const transition = { duration: 2, type: 'spring' };
 
-                <p className='text-[#656B70] xsm:w-full font-normal text-[16px] mdd:text-[14px] xlg:text-[16px] font-Noto w-[90%] leading-[28px]'>{desc}</p>
+  return (
+    <motion.div initial={initialAnimation} whileInView={whileInViewAnimation} transition={transition} className='flex flex-col mdd:flex-row-reverse my-[20px] rounded-[30px] h-auto  mdd:h-[420px] lg:h-[490px] xlg:h-[510px]  xll:h-[490px]  w-full relative items-center justify-center'>
+      <div className='rounded-[20px] rounded-s-[20px] mdd:rounded-s-[0px]  rounded-e-[30px]  h-full w-full my-[40px] mdd:w-[45%] flex flex-col gap-[16px] px-[20px] xsm:px-[20px] xlg:px-[30px] xll:px-[55px] py-[20px] items-start justify-center ssm:py-[60px]  bg-white '>
+        <Image src={logo} loading='lazy' width={100} height={100} className='w-[120px] lg:w-[228px] h-[70px]' alt='Logo' />
+        <h3 className='text-[15px] leading-[38px] ssm:text-[18px] xsm:text-[22px] lg:text-[28px] font-normal font-Montserrat'><span className='text-[#EB2022] font-bold'>{spanTitle}</span>{title}</h3>
+        <p className='text-[#656B70] xsm:w-full font-normal text-[16px] mdd:text-[14px] xlg:text-[16px] font-Noto w-[90%] leading-[28px]'>{desc}</p>
+        <Link className="text-[#2957A7] hover:ml-[20px] hover:text-black  hover:scale-105 duration-[1s]  text-[14px] xsm:text-[16px] font-semibold flex w-[50%] items-center gap-[4px]" href={`/${id}`} > View case <Image src={'/assets/Arrow.svg'} loading='lazy' width={100} height={100} alt='arrow' className='w-[14px] xsm:w-[18px]' /> </Link>
+      </div>
+      <div style={blueGradient} className='overflow-hidden flex items-center justify-center h-full rounded-[20px] mdd:rounded-e-[0px] rounded-s-[20px] mdd:rounded-s-[30px] px-[5px] ssm:px-[70px] py-[80px] ssm:py-[80px] bg-l w-full mdd:w-[55%] lg:w-[60%] '>
+        <Image src={image} loading='lazy' width={500} height={500} className='w-full h-full ssm:w-[90%] ssm:h-auto lg:w-[700px] lg:h-[366px] object-contain' alt='product image' />
+      </div>
+    </motion.div>
+  );
+};
 
-                <Link className="text-[#2957A7] hover:ml-[20px] hover:text-black  hover:scale-105 duration-[1s]  text-[14px] xsm:text-[16px] font-semibold flex w-[50%] items-center gap-[4px]" href={`/${id}`} > View case <Image src={'/assets/Arrow.svg'} width={100} height={100} alt='arrow' className=' w-[14px] xsm:w-[18px]' /> </Link>
-
-
-            </div>
-            <div style={blueGradient} className='overflow-hidden flex items-center justify-center h-full rounded-[20px] mdd:rounded-e-[0px] rounded-s-[20px] mdd:rounded-s-[30px] px-[5px] ssm:px-[70px] py-[80px] ssm:py-[80px]   bg-l w-full mdd:w-[55%] lg:w-[60%] '>
-                <Image src={image} width={1000} height={1000} className=' w-full h-full ssm:w-[90%] ssm:h-auto lg:w-[700px] lg:h-[366px] object-contain' alt='product image' />
-            </div>
-        </motion.div>
-    )
-}
-
-export default ProtfolioCardReverse
+export default ProtfolioCardReverse;
