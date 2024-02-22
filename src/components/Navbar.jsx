4245873918ea2c8f56eeef27a0cf2212758a9d76
Zubair Ahmad <zubairahmad@joyngroup.us>
2024-02-22 12:02:19 +0500

    'ch'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,12 +24,17 @@ const Navbar = () => {
     <div className='h-full flex justify-between items-center w-full max-w-[1440px] px-[60px] lg:px-[90px] xlb:px-[120px] mx-auto '>
     <Link href={'/'} className='relative' >
         <Image
-          src="/assets/logo.png"
+          src="/assets/joyn-png.png"
           width={1000}
           height={1000}
           alt="Picture of the author" priority className='w-[60px] xsm:w-[110px] h-auto object-cover'
         />
-      
+        <Image
+          src="/assets/oimg.png"
+          width={1000}
+          height={1000}
+          alt="Picture of the author" priority className='w-[21px] logo absolute top-0 left-[9px] xsm:left-[19px] xsm:w-[38px] h-auto object-cover'
+        />
       </Link>
 
       <ul className={`flex  items-center flex-col absolute top-0 left-0 transition-all duration-[0.6s] sm:static sm:flex-row w-full h-[100vh] bg-[#F2EEEE] sm:bg-transparent sm:h-auto 
@@ -38,8 +43,8 @@ const Navbar = () => {
     <Link onClick={() => setOpen(false)} className='cursor-pointer  inline-block absolute top-[20px] left-[20px] sm:hidden' href={'/'} >
         <Image
           src="/assets/logo.png"
-          width={200}
-          height={100}
+          width={1000}
+          height={1000}
           alt="Picture of the author" className='w-[60px] xsm:w-[100px] h-auto object-cover'
         />
       </Link>
@@ -47,9 +52,7 @@ const Navbar = () => {
     </div>
         {
           navLinks.map((i) => {
-            return <li key={i.id}>
-               <Link onClick={() => LinkClick(i)}   className={`font-Noto text-[18px] lg:text-[16px] text-[#202020] link font-[400] ${active === i ? 'active' : ''}`} href={i.id}  >{i.title}</Link>
-            </li>
+            return <Link onClick={() => LinkClick(i)}   className={`font-Noto text-[18px] lg:text-[16px] text-[#202020] link font-[400] ${active === i ? 'active' : ''}`} href={i.id} key={i.id} >{i.title}</Link>
 
           })
         }
